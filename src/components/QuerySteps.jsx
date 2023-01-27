b4d467997c692b8bcacd8d114ddc00228a28aee9
rderbier <rderbier@gmail.com>
2023-01-27 10:39:04 -0800

    correct undo fa button

--- src/components/QuerySteps.jsx
+++ src/components/QuerySteps.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/Stack';
-import Accordion from 'react-bootstrap/Accordion';
-import {Exploration, ExplorationStep} from '../services/exploration.js';
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Stack from 'react-bootstrap/Stack'
+import Accordion from 'react-bootstrap/Accordion'
+import {Exploration, ExplorationStep} from '../services/exploration.js'
 import CloseButton from 'react-bootstrap/CloseButton'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUndo } from '@fortawesome/free-solid-svg-icons'
 
 class QuerySteps extends React.Component {
   constructor(props) {
@@ -48,7 +49,8 @@ class QuerySteps extends React.Component {
     }
 
     </Accordion>
-    { (this.props.value.steps.length > 1) && <Button variant="outline" onClick={this.props.undo}><i class="fa fa-undo" /> </Button>}
+
+    { (this.props.value.steps.length > 1) && <FontAwesomeIcon icon={faUndo} onClick={this.props.undo}/>}
 
     </>
   );
